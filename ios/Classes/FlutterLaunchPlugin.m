8112e1b26bb5593fe190149ac6e96deba2e0b0d5
thyagoluciano <thyagoluciano@gmail.com>
2018-06-21 16:35:19 -0300

    github pages

--- ios/Classes/FlutterLaunchPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "FlutterLaunchPlugin.h"
-#import <flutter_launch/flutter_launch-Swift.h>
-
-@implementation FlutterLaunchPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterLaunchPlugin registerWithRegistrar:registrar];
-}
-@end
